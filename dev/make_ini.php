c4b5318df91b5c026a1e984f5d574ac4fff7b8eb
Richard Jones <rich@linuxplicable.org>
2011-01-31 19:08:15 +0000

    php scraper

---
+++ dev/make_ini.php
@@ -0,0 +1,28 @@
+<?PHP
+
+function curl_request($url, array $data = NULL, array $options = array())
+{
+    $defaults = array(
+        CURLOPT_POST => 0,
+        CURLOPT_HEADER => 0,
+        CURLOPT_URL => $url,
+        CURLOPT_FRESH_CONNECT => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_FORBID_REUSE => 1,
+        CURLOPT_TIMEOUT => 4,
+        CURLOPT_POSTFIELDS => http_build_query($data)
+    );
+
+    $ch = curl_init();
+    curl_setopt_array($ch, ($options + $defaults));
+    if( ! $result = curl_exec($ch))
+    {
+        trigger_error(curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}
+
+print_r(curl_request("http://www.guardian.co.uk/crosswords/12107"));
+
+
