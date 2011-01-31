115861d7e3412a1ec52f562c57bb34297d8b3703
Richard Jones <rich@linuxplicable.org>
2011-01-31 20:50:19 +0000

    Guardian scraper

--- dev/make_ini.php
+++ dev/make_ini.php
@@ -1,6 +1,14 @@
 <?PHP
 
-function curl_request($url, array $data = NULL, array $options = array())
+if (count($argv) < 3) {
+	echo $argv[0]." type number\n";
+	exit;
+}
+
+$type = $argv[1];
+$cw = $argv[2];
+
+function curl_request($url)
 {
     $defaults = array(
         CURLOPT_POST => 0,
@@ -10,19 +18,76 @@ function curl_request($url, array $data = NULL, array $options = array())
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_FORBID_REUSE => 1,
         CURLOPT_TIMEOUT => 4,
-        CURLOPT_POSTFIELDS => http_build_query($data)
     );
-
+    
     $ch = curl_init();
-    curl_setopt_array($ch, ($options + $defaults));
+    curl_setopt_array($ch, $defaults);
     if( ! $result = curl_exec($ch))
     {
         trigger_error(curl_error($ch));
+        echo "ERROR";
     }
     curl_close($ch);
     return $result;
 }
 
-print_r(curl_request("http://www.guardian.co.uk/crosswords/12107"));
+function make_ini($arr, $name) {
+	$tmp = "";
+	foreach ($arr as $clue => $data) {
+		$tmp .= '['.$clue."]\n";
+		foreach($data as $key => $var) {
+			$tmp .= $key.'='.$var."\n";
+		}
+		$tmp .= "\n";
+	}
+	file_put_contents($name, $tmp);
+}
+
+if (file_exists($cw)) {
+	$file = file_get_contents($cw);
+} else {
+	$file = curl_request("http://www.guardian.co.uk/crosswords/{$type}/{$cw}");
+	$ret = file_put_contents("./".$cw, $file);
+}
+
+
+//get DIVs
+if (!preg_match_all("/div id=\"([0-9]+-(across|down))\".*style=\"(.*?)\".*>/", $file, $matches)) {
+	echo "Something's wrong, can't get the clues\n";
+}
+
+define("SQUARE_SIZE", 29);
+
+$clues = array();
+foreach($matches[1] as $key => $clue) {
+	preg_match_all("/<input id=\"{$clue}-[0-9]+\"/", $file, $letters);
+	preg_match("/<label id=\"{$clue}-clue\".*?<\/span>\s+(.*?)<\/label>/ms", $file, $cluetext);
+	preg_match("/left: ([0-9]+)px; top: ([0-9]+)px;/", $matches[3][$key], $coords);
+	preg_match_all("/solutions\[\"{$clue}-[0-9]+\"] = \"(.)\"/ms", $file, $solutions);
+	$sol = "";
+	foreach($solutions[1] as $l) {
+		$sol .= $l;
+	}
+	$extra = "";
+	if (preg_match_all("/words_for_clue\[\"{$clue}\"\] = \['{$clue}',(.*)\];/", $file, $words)) {
+		$extra = str_replace("'","", $words[1][0]);
+	}
+	$clues[$clue] = array
+	(
+		'x' => $coords[1]/SQUARE_SIZE,
+		'y' => $coords[2]/SQUARE_SIZE,
+		'length' => count($letters[0]),
+		'clue' => '"'.$cluetext[1].'"',
+		'solution' => '"'.$sol.'"',
+		
+	);
+	if ($extra != "") {
+		$clues[$clue]['extra'] = $extra;
+	}
+}
+
+print_r($clues);
+
+make_ini($clues, $type.'-'.$cw.'.ini');
 
 
