a8fc99573c3d8419ea844f3447b197e8d9d2ce99
Richard <b.mcgavin@gmail.com>
2011-02-01 22:06:36 +0000

    dev to live

--- cw.js
+++ cw.js
@@ -1,7 +1,80 @@
+
+function CrosswordData() {
+	var active_letter;
+	var active_word;
+	var LEFT;
+	var RIGHT;
+	var UP;
+	var DOWN;
+}
+
+CrosswordData.splitWordAndGetLetter = function() {
+	tmp = CrosswordData.active_letter.split('-');
+	return tmp[2];
+}
+
+ 
+var inputBind = function(event) 
+{
+	//$("div#information").html(//$("div#information").html()+new Date().getTime()+":CALL<br/>");
+	letter = CrosswordData.splitWordAndGetLetter();
+
+	if (event.which == CrosswordData.UP || event.which == CrosswordData.DOWN || event.which == CrosswordData.LEFT || event.which == CrosswordData.RIGHT) {
+		new_letter = getSpace(CrosswordData.active_letter, event.which);
+		//$("div#information").html(//$("div#information").html()+new Date().getTime()+":NEW/OLD="+new_letter+"/"+CrosswordData.active_letter+"<br/>");
+		if (new_letter != CrosswordData.active_letter) {
+			CrosswordData.active_letter = moveTo(new_letter, words_in_clue[CrosswordData.active_word]);
+			split_letter = CrosswordData.active_letter.split('-');
+			letter = split_letter[2];
+		}
+	} else if (event.which == 8) {
+		$("input#"+CrosswordData.active_letter).val('');
+		if (intersections[CrosswordData.active_letter]) {
+			$("input#"+intersections[CrosswordData.active_letter]).val('');
+		}
+		alert(letter);
+		alert(words_in_clue[CrosswordData.active_word]);
+		if (letter > 1) {
+			letter--;
+			CrosswordData.active_letter = moveTo(split_word[0]+"-"+split_word[1]+"-"+(parseInt(letter)), words_in_clue[CrosswordData.active_word])
+		} else if (letter == 1 && words_in_clue[CrosswordData.active_word].length > 1) {
+			alert("Old word");
+			$(words_in_clue[CrosswordData.active_word]).each(function (key, word) {
+				if (word != CrosswordData.active_word) {
+					moveTo(word+"-"+lengths[word], CrosswordData.active_word);
+					return false;
+				}
+			});
+		}
+	} else {
+		$("input#"+CrosswordData.active_letter).val(get_letter(event.which));
+		if (intersections[CrosswordData.active_letter]) {
+			$("input#"+intersections[CrosswordData.active_letter]).val(get_letter(event.which));
+		}
+		//move CrosswordData.RIGHT/CrosswordData.DOWN for keypress, CrosswordData.UP/CrosswordData.LEFT for backspace
+		//$("div#information").html($("div#information").html()+new Date().getTime()+":LETT="+letter+"<br/>");
+		
+		if (letter < lengths[CrosswordData.active_word]) {
+			letter++;
+			CrosswordData.active_letter = moveTo(split_word[0]+"-"+split_word[1]+"-"+(parseInt(letter)), words_in_clue[CrosswordData.active_word]);
+		} else if (letter == lengths[CrosswordData.active_word] && words_in_clue[CrosswordData.active_word].length > 1) {
+			$(words_in_clue[CrosswordData.active_word]).each(function (key, word) {
+				if (word != CrosswordData.active_word) {
+					moveTo(word+"-1", CrosswordData.active_word);
+					return false;
+				}
+			});
+		}
+	}
+	return false;
+}
+
+var words = {};
+
 function processOne(how) {
-	if (typeof(active_word) != 'undefined' && active_word != "" && lengths[active_word]) {
-		for (i = 1; i <= lengths[active_word]; i++) {
-			key = active_word+"-"+i;
+	if (typeof(CrosswordData.active_word) != 'undefined' && CrosswordData.active_word != "" && lengths[CrosswordData.active_word]) {
+		for (i = 1; i <= lengths[CrosswordData.active_word]; i++) {
+			key = CrosswordData.active_word+"-"+i;
 			if (how == 'cheat') {
 				$("input#"+key).val(solutions[key]);
 				if (intersections[key]) {
@@ -20,29 +93,25 @@ function processOne(how) {
 }
 
 function processAll(how) {
-	if (typeof(active_word) != 'undefined') {
-		old_active_word = active_word;
+	if (typeof(CrosswordData.active_word) != 'undefined') {
+		old_active_word = CrosswordData.active_word;
 		for(solution in solutions) {
 			solution_split = solution.split('-');
-			active_word = solution_split[0]+"-"+solution_split[1];
+			CrosswordData.active_word = solution_split[0]+"-"+solution_split[1];
 			processOne(how);
 		}
-		active_word = old_active_word;
+		CrosswordData.active_word = old_active_word;
 	}
 }
 
 function getSpace(from, direction) {
-	var LEFT = 37;
-	var RIGHT = 39;
-	var UP = 38;
-	var DOWN = 40;
 	var old_from = from;
 	split_from = from.split('-');
 	//if there's an intersection and the direction is perpendicular to the current clue
 	if (
 		(
-			(split_from[1] == 'across' && (direction == UP || direction == DOWN)) ||
-			(split_from[1] == 'down' && (direction == LEFT || direction == RIGHT))
+			(split_from[1] == 'across' && (direction == CrosswordData.UP || direction == CrosswordData.DOWN)) ||
+			(split_from[1] == 'down' && (direction == CrosswordData.LEFT || direction == CrosswordData.RIGHT))
 		) && intersections[split_from[0]+"-"+split_from[1]+"-"+split_from[2]]
 	) {
 		//$("div#information").html(//$("div#information").html()+new Date().getTime()+":INT"+from);
@@ -51,23 +120,24 @@ function getSpace(from, direction) {
 		if (
 			(
 				(
-					(split_from[1] == 'across' && direction == LEFT) || 
-					(split_from[1] == 'down' && direction == UP)
+					(split_from[1] == 'across' && direction == CrosswordData.LEFT) || 
+					(split_from[1] == 'down' && direction == CrosswordData.UP)
 				) && parseInt(split_from[2]) == 1
 			) || (
 				(
-					(split_from[1] == 'across' && direction == RIGHT) || 
-					(split_from[1] == 'down' && direction == DOWN)
+					(split_from[1] == 'across' && direction == CrosswordData.RIGHT) || 
+					(split_from[1] == 'down' && direction == CrosswordData.DOWN)
 				) && parseInt(split_from[2]) == lengths[split_from[0]+"-"+split_from[1]]
 			)
 		) {
 			//$("div#information").html(//$("div#information").html()+new Date().getTime()+":NOMOVE"+from);
+			//TODO is there an extra word in this clue?
 			return from;
 		}
-		if ((split_from[1] == 'across' && direction == LEFT) || (split_from[1] == 'down' && direction == UP)) {
+		if ((split_from[1] == 'across' && direction == CrosswordData.LEFT) || (split_from[1] == 'down' && direction == CrosswordData.UP)) {
 			//$("div#information").html(//$("div#information").html()+new Date().getTime()+":BACK"+from);
 			return split_from[0]+"-"+split_from[1]+"-"+(parseInt(split_from[2])-1);
-		} else if ((split_from[1] == 'across' && direction == RIGHT) || (split_from[1] == 'down' && direction == DOWN)) {
+		} else if ((split_from[1] == 'across' && direction == CrosswordData.RIGHT) || (split_from[1] == 'down' && direction == CrosswordData.DOWN)) {
 			//$("div#information").html(//$("div#information").html()+new Date().getTime()+":FORWARD"+from);
 			return split_from[0]+"-"+split_from[1]+"-"+(parseInt(split_from[2])+1);
 		}
@@ -90,65 +160,70 @@ function checkSpace(place) {
 function moveTo(new_letter, old_word) {
 	//$("div#information").html(//$("div#information").html()+new Date().getTime()+":KEY"+new_letter+"<br/>");
 	$("input#"+new_letter).focus();
-	$("input#"+new_letter).select();
 	split_letter = new_letter.split('-');
 	word = split_letter[0]+"-"+split_letter[1];
+	CrosswordData.active_letter = new_letter;
 	if (word != old_word) {
 		//$("div#information").html(//$("div#information").html()+new Date().getTime()+":HLT"+word+"/"+split_letter[2]+"<br/>");
+		clearAllExcept([word]);
 		highlightWord(word, split_letter[2]);
 	}
 	//$("div#information").html($("div#information").html()+new Date().getTime()+":DST"+new_letter+"<br/>");
 	return new_letter;
 }
 
+function clearAllExcept(exceptions) {
+	tmp = [];
+	if (typeof(exceptions) == 'object') {
+		tmp.push(exceptions[0]);
+	}
+	avoid = "not:(#"+tmp.join("):not:(#")+")";
+	$("div#crossword div:"+avoid+" > input").removeClass("highlight").css("z-index",split_word[0]);
+//.unbind('keyCrosswordData.UP');
+	avoid = "not:(#"+tmp.join("-clue):not:(#")+"-clue)";
+	$("div.clue:"+avoid).removeClass("darken");
+}
+
 function highlightWord(word, letter) {
-	active_word=word;
-	var LEFT = 37;
-	var RIGHT = 39;
-	var UP = 38;
-	var DOWN = 40;
+	CrosswordData.active_word=word;
 	////$("div#information").html(//$("div#information").html()+new Date().getTime()+":TOP"+word+"-"+letter+"<br/>");
-	var active_letter = word+"-"+letter;
+	CrosswordData.active_letter = word+"-"+letter;
 	split_word = word.split('-');
-	//remove highlight from all other elements
-	$("div#crossword input").removeClass("highlight").css("z-index",split_word[0]);
-	$("div.clue").removeClass("darken");
-	$("input").unbind('keyup');
-	$("div#"+word+".word input").addClass("highlight").css("z-index","99");
-	$("div#"+word+"-clue").addClass("darken");
-	$("div#"+word+".word input").keyup(function(event) {
-		//$("div#information").html(//$("div#information").html()+new Date().getTime()+":CALL<br/>");
-		var old_word = word;
-		if (event.which == UP || event.which == DOWN || event.which == LEFT || event.which == RIGHT) {
-			new_letter = getSpace(active_letter, event.which);
-			//$("div#information").html(//$("div#information").html()+new Date().getTime()+":NEW/OLD="+new_letter+"/"+active_letter+"<br/>");
-			if (new_letter != active_letter) {
-				active_letter = moveTo(new_letter, word);
-				split_letter = active_letter.split('-');
-				letter = split_letter[2];
+	
+	clearAllExcept(words_in_clue[word]);
+	clue = "";
+	for (tmp in words_in_clue[word]) {
+		if (typeof(words_in_clue[word][tmp]) != 'undefined') {
+			//alert(words[word]);
+			$("div#"+words_in_clue[word][tmp]+".word input").addClass("highlight").css("z-index","99");
+			$("div#"+words_in_clue[word][tmp]+"-clue").addClass("darken");
+			clue += $("div#"+words_in_clue[word][tmp]+"-clue").html()+"\n</br>";
+		}
+	}
+	$("div#active-clue").html(clue);
+	//search through all of words_in_clue to see if this word is in any others
+	//TODO Make this a jQ method call so it can be incorporated into the backspace multi-word function above.
+	for (tmp in words_in_clue) {
+		if (typeof(words_in_clue[tmp]) != 'undefined') {
+			if (tmp != word && words_in_clue[tmp].length > 1) {
+				doneit = false;
+				for (tmptmp in words_in_clue[tmp]) {
+					if (typeof(words_in_clue[tmp][tmptmp]) != 'undefined') {
+						if (words_in_clue[tmp][tmptmp] == word)	{
+							for (doit in words_in_clue[tmp]) {
+								$("div#"+words_in_clue[tmp][doit]+".word input").addClass("highlight").css("z-index","99");
+								$("div#"+words_in_clue[tmp][doit]+"-clue").addClass("darken");
+								doneit = true;
+							}
+							if (doneit) {
+								break;
+							}
+						}
+					}
+				}
 			}
-		} else if (event.which == 8) {
-			$("input#"+active_letter).val('');
-			if (intersections[active_letter]) {
-				$("input#"+intersections[active_letter]).val('');
-			}
-			if (letter > 1) {
-				letter--;
-				active_letter = moveTo(split_word[0]+"-"+split_word[1]+"-"+(parseInt(letter)), word)
-			}
-		} else {
-			$("input#"+active_letter).val(get_letter(event.which));
-			if (intersections[active_letter]) {
-				$("input#"+intersections[active_letter]).val(get_letter(event.which));
-			}
-			//move right/down for keypress, up/left for backspace
-			//$("div#information").html($("div#information").html()+new Date().getTime()+":LETT="+letter+"<br/>");
-			if (letter < lengths[word]) {
-				letter++;
-				active_letter = moveTo(split_word[0]+"-"+split_word[1]+"-"+(parseInt(letter)), word);
-			}
-		}	
-	});
+		}
+	}
 }
 
 function get_letter(which) {
