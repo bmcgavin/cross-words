c617f12b021bfd3d4e1b71ed7481ab9cadfc9f61
Richard <b.mcgavin@gmail.com>
2011-02-05 13:50:56 +0000

    [dev] customised double.php

--- double.php
+++ double.php
@@ -7,10 +7,64 @@
 	</head>
 	<body>
 <?PHP
-	$rand = rand(9100,11000);
+	$whens = array(
+		'first' => 1,
+		'latest' => 1,
+		'random' => 1,
+	);
+	$rand = array(
+		'quick' => rand(9100,11000),
+		'cryptic' => rand(24500, 25000),
+		'hc' => rand(1, 2)
+	);
+	$valid_types = array_keys($rand);
+	$first_type = "quick";
+	$first_when = "latest";
+	$second_type = "quick";
+	$second_when = "random";
+	if (@$_GET['first_type'] && array_key_exists($_GET['first_type'], $rand)) {
+		$first_type = $_GET['first_type'];
+	}
+	if (@$_GET['second_type'] && array_key_exists($_GET['second_type'], $rand)) {
+		$second_type = $_GET['second_type'];
+	}
+	if (@$_GET['first_when'] && (
+		is_numeric($_GET['first_when']) || array_key_exists($_GET['first_when'], $whens)
+	)) {
+		$first_when = $_GET['first_when'];
+	}
+	if (@$_GET['second_when'] && (
+		is_numeric($_GET['second_when']) || array_key_exists($_GET['second_when'], $whens)
+	)) {
+		$second_when = $_GET['second_when'];
+	}
+	
+	function get_random_for_type($type, $rand) {
+		$ret = $rand[$type];
+		while (!file_exists("ini/{$type}-{$rand}.ini")) {
+			$rand = array(
+				'quick' => rand(9100,11000),
+				'cryptic' => rand(24500, 25000),
+				'hc' => rand(1, 2)
+			);
+			$ret = $rand[$type];
+		}
+		return $ret;
+	}
+	
+	if ($first_when == 'random') {
+		$first_when = get_random_for_type($first_type, $rand);
+	}
+	$first = $first_type.'-'.$first_when;
+	
+	if ($second_when == 'random') {
+		$second_when = get_random_for_type($second_type, $rand);
+	}
+	$second = $second_type.'-'.$second_when;
+
 ?>
-		<iframe src="cw.php?cw=quick-latest" width="100%" height="700px"></iframe>
-		<iframe src="cw.php?cw=quick-<?=$rand?>" width="100%" height="700px"></iframe>
+		<iframe src="cw.php?cw=<?=$first?>" width="100%" height="700px"></iframe>
+		<iframe src="cw.php?cw=<?=$second?>" width="100%" height="700px"></iframe>
 		
 	</body>
 </html>
