42ddaa21b73d4b42b7fb87abfaa6dcf2a1ced4c9
Richard <b.mcgavin@gmail.com>
2011-02-05 21:47:40 +0000

    [dev] hyphens

--- dev/cw.php
+++ dev/cw.php
@@ -172,9 +172,9 @@ EOF;
 		if (array_key_exists('word_hyphens', $data) && in_array($letter, $data['word_hyphens']) && $letter != $length) {
 			//OUTPUT A HYPHEN DIV
 			if ($dir == 'across') {
-				$style = "left:{$left}px";
+				$style = "left:{$clue_left}px";
 			} else if ($dir == 'down') {
-				$style = "top:{$top}px";
+				$style = "top:{$clue_top}px";
 			}
 			$output .= <<< EOF
 			<div class="hyphen-{$dir}" style="{$style};">&nbsp;</div>
